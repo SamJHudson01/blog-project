86d70e3a2676d8a98b992bd092ce143b657e6104
SamJHudson01 <samjhudson@gmail.com>
2024-06-04 13:19:54 +0100

    create signup route/controller

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -13,8 +15,16 @@ mongoose
     console.log("Error connecting to the database: " + error);
   });
 
+
+
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
+
+//use the userRouter
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
