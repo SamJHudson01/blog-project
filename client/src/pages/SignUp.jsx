60bdd6a0e28cc5fcc8192974821de26e0b63f52b
SamJHudson01 <samjhudson@gmail.com>
2024-06-04 15:41:06 +0100

    create signup page design

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,69 @@
-const SignUp = () => {
+import { Button, Label, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
+
+export default function SignUp() {
   return (
-    <div>
-      <h1>Sign Up</h1>
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Renegade
+            </span>
+            React
+          </Link>
+          <p className="text-sm mt-5">
+            Javascript and React articles for cool kids.
+          </p>
+        </div>
+        {/* right */}
+
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={() => {}}>
+            <div>
+              <Label value="Your username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={() => {}}
+              />
+            </div>
+            <div>
+              <Label value="Your email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={() => {}}
+              />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={() => {}}
+              />
+            </div>
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={() => {}}
+            >
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
-
-export default SignUp;
+}
