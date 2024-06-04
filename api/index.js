558e918d9e8edf92264c86917ae85e14030fcb73
SamJHudson01 <samjhudson@gmail.com>
2024-06-04 10:30:55 +0100

    set up database connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.log("Error connecting to the database: " + error);
+  });
 
 const app = express();
 
